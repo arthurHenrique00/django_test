aad5c618de1355095b878bae9ee8cc244040ae9f
arthurHenrique00 <arthurcostalima88@gmail.com>
2025-03-13 02:35:07 -0300

    Modelo exercicio

--- django_test/blog/models.py
+++ django_test/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Projeto(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.TextField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
+
